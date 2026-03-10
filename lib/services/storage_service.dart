55984da15a522e7f246eec706206b1b3a40baac1
adjaFatouG26 <adjifatougueye89@gmail.com>
2026-03-10 08:39:08 +0000

    feat: ajout des methodes utilisateur dans StorageService

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,5 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'dart:convert';
+import 'package:sunu_task/models/User.dart';
+
 /**
  * Pattern Singleton:
  * Pour avoir une seule instance
@@ -46,4 +49,53 @@ class StorageService {
     return _prefs.getBool(_keyOnboardingConmplete) ?? false;
   }
 
+   Future<void> setOnboardingComplete(bool value) async {
+    await _prefs.setBool(_keyOnboardingConmplete, value);
+  }
+
+//ici on crée un clé utilisateur
+
+  static const String _keyUsers = 'users';
+  static const String _keyCurrentUser = 'current_user';
+
+  // aprés on récupére ts les utilisateurs
+
+  List<User> getUsers() {
+    final data = _prefs.getString(_keyUsers);
+    if (data == null) return [];
+    final List<dynamic> list = jsonDecode(data);
+    return list.map((e) => User.fromMap(e)).toList();
+  }
+  // on sauvegarde un utilisateur
+
+  Future<void> saveUser(User user) async {
+    final users = getUsers();
+    final index = users.indexWhere((u) => u.id == user.id);
+    if (index >= 0) {
+      users[index] = user;
+    } else {
+      users.add(user);
+    }
+    final list = users.map((u) => u.toMap()).toList();
+    await _prefs.setString(_keyUsers, jsonEncode(list));
+  }
+  // enfin ici on récupére les utilisateurs courant
+
+  User? getCurrentUser() {
+    final data = _prefs.getString(_keyCurrentUser);
+    if (data == null) return null;
+    return User.fromMap(jsonDecode(data));
+  }
+
+  // Sauvegarder l'utilisateur courant
+
+  Future<void> saveCurrentUser(User user) async {
+    await _prefs.setString(_keyCurrentUser, jsonEncode(user.toMap()));
+  }
+
+  // Supprimer l'utilisateur courant
+
+  Future<void> clearCurrentUser() async {
+    await _prefs.remove(_keyCurrentUser);
+  }
 }
