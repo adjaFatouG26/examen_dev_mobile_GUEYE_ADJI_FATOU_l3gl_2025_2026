bd6b66433e8cd67c44ae88ae459a78c624b4e172
adjaFatouG26 <adjifatougueye89@gmail.com>
2026-03-11 04:37:05 +0000

    feat: ajout providers dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:sunuTask/core/theme/app_theme.dart';
-import 'package:sunuTask/screens/splash/splash_screen.dart';
-import 'package:sunuTask/services/storage_service.dart';
+import 'package:sunu_task/core/theme/app_theme.dart';
+import 'package:sunu_task/screens/splash/splash_screen.dart';
+import 'package:sunu_task/services/storage_service.dart';
+import 'package:sunu_task/providers/app_provider.dart';
+import 'package:sunu_task/providers/auth_provider.dart';
+import 'package:sunu_task/providers/project_provider.dart';
+import 'package:sunu_task/providers/task_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +15,27 @@ void main() async {
   runApp(const SunuTask());
 }
 
-class SunuTask extends StatelessWidget {
+class SunuTask extends StatefulWidget {
   const SunuTask({super.key});
+
+  @override
+  State<SunuTask> createState() => _SunuTaskState();
+}
+
+class _SunuTaskState extends State<SunuTask> {
+  // Déclaration des providers
+  final _appProvider = AppProvider();
+  final _authProvider = AuthProvider();
+  final _projectProvider = ProjectProvider();
+  final _taskProvider = TaskProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialisation des providers
+    _appProvider.init();
+    _authProvider.init();
+  }
 
   // This widget is the root of your application.
   @override
